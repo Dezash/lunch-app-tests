428c9929e071e9757660dc33911fba1d8ea0a875
Gabrielius Laurinavičius <gabrielius.laurinavicius@gmail.com>
2020-09-14 16:50:22 +0300

    Add environment varialbes

--- cypress/integration/tests/tests.spec.js
+++ cypress/integration/tests/tests.spec.js
@@ -5,8 +5,8 @@ var selector = new selectors
 
 beforeEach('Executes  before each test', () =>{
     cy.visit('https://lunch.devbstaging.com/login-password')
-    cy.get(':nth-child(1) > .v-input__control > .v-input__slot > .v-text-field__slot > input').type('admin@yellow.kns')
-    cy.get(':nth-child(2) > .v-input__control > .v-input__slot > .v-text-field__slot > input').type('Corn000')
+    cy.get(':nth-child(1) > .v-input__control > .v-input__slot > .v-text-field__slot > input').type(Cypress.env('email'))
+    cy.get(':nth-child(2) > .v-input__control > .v-input__slot > .v-text-field__slot > input').type(Cypress.env('password'))
     cy.get('.v-btn').click()
     cy.get('.d-flex > .v-subheader').contains('Admin Yellow')
 })
